8848401ff8e4b687d2d536b274f034754ed219bd
Ankit Kushwaha <er.ankitkush@gmail.com>
2023-10-19 00:53:01 +0530

    action enable

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-scroll';
-import {HiMenu} from 'react-icons/hi'
 import './Navbar.css';
 
 function Navbar() {
@@ -8,7 +7,16 @@ function Navbar() {
             <div className="w-full md:h-full px-6 md:px-[6rem] flex justify-between items-center">
                 <img id='logoimage' src="https://i.ibb.co/GVYVn4w/image-removebg-preview.png" alt="Logon" className="h-14 md:h-24" />
                 <div className='md:hidden'>
-                    <HiMenu size={30}/>
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Homepage</a></li>
+                            <li><a>Portfolio</a></li>
+                            <li><a>About</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div className='hidden md:flex gap-4'>
                     <div className='hover:animate-bounce cursor-pointer'><Link to="Home" smooth duration={500}>Home</Link></div>
